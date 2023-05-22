b73ff9cbf6da782ed43351615517ece847149786
Nestor162 <nestorcicardini@gmail.com>
2023-05-22 14:24:56 +0200

    Importato logger

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,18 +2,17 @@ package app;
 
 import javax.persistence.EntityManagerFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import util.JpaUtil;
 
 public class Main {
 
 	private static EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+	private static Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("PROVA SONO MATTEO");
-		System.out.println("djddiee");
-
-		System.out.println("dd");
 
 	}
 
