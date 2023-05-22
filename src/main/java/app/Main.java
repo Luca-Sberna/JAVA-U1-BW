1dfcaf005c72a415f516557cdb756f2fa42487db
matteovcc <matteovcc99@gmail.com>
2023-05-22 10:53:44 +0200

    first commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+import javax.persistence.EntityManagerFactory;
+
+import util.JpaUtil;
+
+public class Main {
+
+	private static EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("PROVA SONO MATTEO");
+
+	}
+
+}
